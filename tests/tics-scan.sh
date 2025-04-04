f6400630d541d52533fe98e2ddff786e50a4daa1
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-04-04 10:42:56 +0000

    donotmerge: TICS tests

--- tests/tics-scan.sh
+++ tests/tics-scan.sh
@@ -30,16 +30,16 @@ gocover-cobertura < coverage.txt > coverage.xml
 mkdir -p .coverage
 mv ./coverage.xml ./.coverage/
 
-# Install the TICS and staticcheck
-go install honnef.co/go/tools/cmd/staticcheck@v0.5.1
-. <(curl --silent --show-error 'https://canonical.tiobe.com/tiobeweb/TICS/api/public/v1/fapi/installtics/Script?cfg=default&platform=linux&url=https://canonical.tiobe.com/tiobeweb/TICS/')
+# # Install the TICS and staticcheck
+# go install honnef.co/go/tools/cmd/staticcheck@v0.5.1
+# . <(curl --silent --show-error 'https://canonical.tiobe.com/tiobeweb/TICS/api/public/v1/fapi/installtics/Script?cfg=default&platform=linux&url=https://canonical.tiobe.com/tiobeweb/TICS/')
 
-# We need to have our project built
-# We load the dqlite libs here instead of doing through make because TICS
-# will try to build parts of the project itself
-sudo add-apt-repository -y ppa:dqlite/dev
-sudo apt-get install -y dqlite-tools-v2 libdqlite1.17-dev
-sudo make clean
-go build -a ./...
+# # We need to have our project built
+# # We load the dqlite libs here instead of doing through make because TICS
+# # will try to build parts of the project itself
+# sudo add-apt-repository -y ppa:dqlite/dev
+# sudo apt-get install -y dqlite-tools-v2 libdqlite1.17-dev
+# sudo make clean
+# go build -a ./...
 
-TICSQServer -project k8s-snap -tmpdir /tmp/tics -branchdir $PROJECT_BASE_DIR
+# # TICSQServer -project k8s-snap -tmpdir /tmp/tics -branchdir $PROJECT_BASE_DIR
